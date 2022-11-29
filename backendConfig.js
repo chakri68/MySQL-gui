29e98b201e083e05ffd3658b376638b85c09a277
flyingdelta <flyingdelta@outlook.com>
2022-11-29 11:10:50 +0530

    Added comments to backendConfig

--- backendConfig.js
+++ backendConfig.js
@@ -1,17 +1,25 @@
 // Primary Keys are required for deleting rows
 
 module.exports = {
+  // The port the server should listen at
   port: 3000,
+  // The database to connect to
+  database: {
+    // The hostname
+    host: "localhost",
+    // User
+    user: "root",
+    // Password for the user
+    password: "2004",
+    // The database name
+    database: "company",
+  },
+  // The keys that uniquely identify rows of each table in the database
   primaryKeys: {
+    // [table_name]: "[column_name]"
     department: "Dnumber",
     employee: "SSN",
     project: "Pnumber",
     works_on: "ESSN",
-  },
-  database: {
-    host: "localhost",
-    user: "root",
-    password: "2004",
-    database: "company",
   },
 };
