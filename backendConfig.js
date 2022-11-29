f033a45d429324e85688285d5288eb73a8d67256
flyingdelta <flyingdelta@outlook.com>
2022-11-29 08:14:51 +0530

    Added database details, port to backendConfig file

--- backendConfig.js
+++ backendConfig.js
@@ -1,10 +1,17 @@
 // Primary Keys are required for deleting rows
 
 module.exports = {
+  port: 3000,
   primaryKeys: {
     department: "Dnumber",
     employee: "SSN",
     project: "Pnumber",
     works_on: "ESSN",
+  },
+  database: {
+    host: "localhost",
+    user: "root",
+    password: "2004",
+    database: "company",
   },
 };
--- server.js
+++ server.js
@@ -7,16 +7,19 @@ const {
   deleteRow,
 } = require("./funcs");
 const app = express();
-const port = 3000;
 const cors = require("cors");
+const backendConfig = require("./backendConfig");
 app.use(cors());
 app.use(express.json());
 
+const { database } = backendConfig.database;
+const port = backendConfig?.port || 3000;
+
 const con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "2004",
-  database: process.argv.slice(2)[0] || "company",
+  host: database?.host || "localhost",
+  user: database?.user || "root",
+  password: database?.password || "2004",
+  database: database?.database || process.argv.slice(2)[0],
 });
 
 con.connect(function (err) {
